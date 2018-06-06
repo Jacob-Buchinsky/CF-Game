643b5ae8d5b0b4913127fcede10c7ebb5a3402e4
Jacob-Buchinsky <jacob.buchibnsky@gmail.com>
2018-06-06 13:19:29 -0400

    Added basic Camera controls
    Camera parameters can be changed in the inspector

---
+++ CF-Game-git/Assets/_Scripts/Tools/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float PanSpeed = 20f;
+    public float panBorderThickness = 10;
+    public Vector2 panLimit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness){
+            pos.z += PanSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= PanSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += PanSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= PanSpeed * Time.deltaTime;
+        }
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+        transform.position = pos;
+	}
+
+}
