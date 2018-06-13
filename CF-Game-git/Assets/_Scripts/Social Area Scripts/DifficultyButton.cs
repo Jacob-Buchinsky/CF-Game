47bbf981b9db68cc9c0c1f4aef3ef0de988c3e26
Jacob-Buchinsky <jacob.buchibnsky@gmail.com>
2018-06-13 16:31:45 -0400

    Mission Select functionality
    Added 3 buttons for difficulty, test mission only appears on easy. Loads
    Test mission when pressed. Added light source to test mission.

---
+++ CF-Game-git/Assets/_Scripts/Social Area Scripts/DifficultyButton.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class DifficultyButton : MonoBehaviour
+{
+    public Button easybutton;
+    public Button mediumbutton;
+    public Button hardbutton;
+    public GameObject Mission;
+    void Start()
+    {
+        
+        Button btnE = easybutton.GetComponent<Button>();
+        Button btnM = mediumbutton.GetComponent<Button>();
+        Button btnH = hardbutton.GetComponent<Button>();
+        //Calls the TaskOnClick method when you click the Button
+        btnE.onClick.AddListener(TaskOnClickEasy);
+        btnM.onClick.AddListener(TaskOnClickMedium);
+        btnH.onClick.AddListener(TaskOnClickHard);
+
+    }
+
+    void TaskOnClickEasy()
+    {
+        //Output this to console when the Button is clicked
+        Debug.Log("You have clicked the button!");
+        Mission.SetActive(true);
+        }
+        
+    void TaskOnClickMedium()
+{
+    //Output this to console when the Button is clicked
+    Debug.Log("You have clicked the button!");
+    Mission.SetActive(false);
+}
+    void TaskOnClickHard()
+    {
+    //Output this to console when the Button is clicked
+    Debug.Log("You have clicked the button!");
+    Mission.SetActive(false);
+    }
+
+}
+
+
---
+++ CF-Game-git/Assets/_Scripts/Tactics/SceneControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControllerTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("Social Hub"));
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("TestMission"));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
